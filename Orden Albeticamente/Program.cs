08e28b0006df21d9918a938f14778593af80cc8e
pcarcamo <paovargas810@gmail.com>
2016-08-07 19:54:20 -0600

    Ejercicio 4

---
+++ Orden Albeticamente/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orden_Albeticamente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra:");
+            string palabra = Console.ReadLine();
+            char[] arregloCaracteres = new char[palabra.Length];
+            Console.WriteLine("Palabra ingresada es : {0}", palabra);
+            palabra.CopyTo(0, arregloCaracteres, 0, arregloCaracteres.Length);
+            Array.Sort(arregloCaracteres);
+            Console.WriteLine("El orden alfabeticamente es: ");
+            for (int i = 0; i < arregloCaracteres.Length; i++)
+                Console.Write(arregloCaracteres[i]);
+            Console.ReadLine();
+
+        }
+    }
+}
