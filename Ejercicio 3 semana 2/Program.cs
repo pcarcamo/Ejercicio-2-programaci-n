b660761512a7873cf9ae1808651090c150ff7aca
pcarcamo <paovargas810@gmail.com>
2016-08-07 19:53:08 -0600

    Ejercicio 3
    Este ejercicio me dio problema

---
+++ Ejercicio 3 semana 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class alfabeticamente
+{
+    static void Main(string[] args)
+    {
+        var Text = Console.ReadLine();
+        var Letras = Text.ToCharArray();
+        var Cantidad = Letras.Count;
+        for (int I = 0; I < Letras.count; I ++)
+        {
+            Console.WriteLine(Letras[I]);
+            if (I < Cantidad - 1)
+                Console.WriteLine(Letras[I] + Letras[I + 1]);
+        }
+        if (I < Cantidad - 2)
+            Console.WriteLine(Letras[I] + Letras[I + 1] + Letras[I + 2]);
+    }
+}
+
+
