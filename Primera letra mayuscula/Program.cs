728c04362522a36258aedd86b439592672ee77f8
pcarcamo <paovargas810@gmail.com>
2016-08-07 19:55:53 -0600

    Ejercicio 5

---
+++ Primera letra mayuscula/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primera_letra_mayuscula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese palabra:");
+            string palabra = Console.ReadLine();
+
+            Console.WriteLine("El texto es: {0}",
+              palabra);
+
+            Console.WriteLine("En mayúsculas: {0}",
+              palabra.ToUpper());
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
