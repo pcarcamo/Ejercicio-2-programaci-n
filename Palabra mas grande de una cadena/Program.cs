134e1b596a17abad65043e95f19cabc8dba16e09
pcarcamo <paovargas810@gmail.com>
2016-08-07 19:55:02 -0600

    Ejercicio 6

---
+++ Palabra mas grande de una cadena/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palabra_mas_grande_de_una_cadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabraGrande = "";
+            int palabra = 0;
+            while (true)
+            {
+                Console.Write("Ingrese una oración ");
+                palabraGrande = Console.ReadLine();
+                if (palabraGrande == "")
+                    break;
+                if (palabra > palabraGrande.Length)
+                    palabraGrande = palabra;
+            }
+            Console.WriteLine("palabra mas larga fue: {0}", palabraGrande);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
