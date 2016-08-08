f79ce5518d3eff59a7b8fde2a3648c11edd7bd48
pcarcamo <paovargas810@gmail.com>
2016-08-07 19:53:44 -0600

    Ejercicio 8

---
+++ Numeros primos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, numero, n;
+            Console.WriteLine("Ingrese un numero");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (numero = 1; nuemro < (n + 1); numero++)
+            {
+                if (n % numero == 0)
+                {
+                    a++;
+                }
+            }
+            if (a != 2)
+            {
+                Console.WriteLine("El número no es Primo");
+            }
+            else
+            {
+                Console.WriteLine("El ´número es Primo");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+      
