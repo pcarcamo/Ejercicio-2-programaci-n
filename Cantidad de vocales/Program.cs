4667ef476dbe4170b739ae75ac89299ce581c713
pcarcamo <paovargas810@gmail.com>
2016-08-07 19:48:30 -0600

    Ejercicio 7

---
+++ Cantidad de vocales/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cantidad_de_vocales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            int contador1 = 0, contador2 = 0, consonantes;
+            Console.WriteLine("Escribe una oración");
+            palabra = Console.ReadLine();
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                contador1++;
+            }
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if ((palabra[i] == 'a') || (palabra[i] == 'e') || (palabra[i] == 'i') || (palabra[i] == 'o') || (palabra[i] == 'u'))
+                {
+                    contador2++;
+                }
+            }
+            consonantes = contador1 - contador2;
+            Console.WriteLine("La oración  tiene:  " + contador2 + " vocales");
+            Console.ReadLine();
+        }
+    }
+
+
+}
+    
+
